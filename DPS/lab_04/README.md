11bc07f8095ff31550f1883dfa3dfd8e6eb43f90
igorshvch <igorshvch@gmail.com>
2022-06-14 11:03:45 +0300

    DPS/fix: lab_04 fixed

--- DPS/lab_04/README.md
+++ DPS/lab_04/README.md
@@ -4,12 +4,14 @@
 
 Примеры результатов работы программы (размеры картинок одинаковы, изменены атвоматическим сжатием md-процессора)
 
-|№|Детектор|bird_small.jpg - бинаризированное|bl2049_small.jpg - в оттенках серого|
+|№|Детектор|Изображение|Параметр порогового значения|
 |-|-|-|-|
-|1|Исходное изображение|![1](../static/bird_small.jpg)|![1](../static/bl2049_small.jpg)|
-|2|Детектор Кэнни|![2](./save/bird_small__Canny_937113.jpg)|![2](./save/bl2049_small_grayscale_Canny_030471.jpg)|
-|3|Детектор Робертса|![3](./save/bird_small__Roberts_8272.jpg)|![3](./save/bl2049_small_grayscale_Roberts_513096.jpg)|
-|4|Детектор Прюитта|![4](./save/bird_small__Prewitt_034653.jpg)|![4](./save/bl2049_small_grayscale_Prewitt_44937.jpg)|
-|5|Детектор Собеля|![5](./save/bird_small__Sobel_53308.jpg)|![5](./save/bl2049_small_grayscale_Sobel_577631.jpg)|
-|6|Детектор Щара|![6](./save/bird_small__Scharr_085255.jpg)|![6](./save/bl2049_small_grayscale_Scharr_745421.jpg)|
-|7|Детектор Лапласа-Гаусса|![7](./save/bird_small__Laplace_461749.jpg)|![7](./save/bl2049_small_grayscale_Laplace_929457.jpg)|
+|1|Исходное изображение|![1](../static/bl2049_small.jpg)|...|
+|2|Детектор Кэнни|![2](./save/bl2049_small_t20_Canny_972171.jpg)|Нижний - 50, верхний - 150|
+|3|Детектор Робертса|![3](./save/bl2049_small_t20_Roberts_767132.jpg)|20|
+|4|Детектор Прюитта|![4](./save/bl2049_small_t20_Prewitt_388585.jpg)|20|
+|5|Детектор Собеля|![5](./save/bl2049_small_t20_Sobel_67346.jpg)|20|
+|6|Детектор Щара|![6](./save/bl2049_small_t20_Scharr_791589.jpg)|20|
+|7|Детектор Щара|![7](./save/bl2049_small_t60_Scharr_255874.jpg)|60)|
+|8|Детектор Лапласа-Гаусса|![8](./save/bl2049_small_t4_Laplace_47323.jpg)|0.04|
+|9|Детектор Лапласа-Гаусса|![9](./save/bl2049_small_t8_Laplace_37641.jpg)|0.08|
--- DPS/lab_04/lab_04.py
+++ DPS/lab_04/lab_04.py
@@ -1,3 +1,4 @@
+from concurrent.futures import thread
 import pathlib as path
 from datetime import datetime as dt
 
@@ -29,9 +30,37 @@ def get_bin_img(g_img):
     _, b_img = cv2.threshold(g_img, 0, 255, cv2.THRESH_OTSU)
     return b_img
 
-def edge_detector(b_img, mode):
+def find_zero_cross(LoG_img, d):
+    width, height = LoG_img.shape
+    tem_img = np.zeros((width+2, height+2), np.float32)
+    for i in range(1, width+1):
+        for j in range(1, height+1):
+            tem_img[i,j] = LoG_img[i-1,j-1]
+    res_img = np.zeros((width, height), np.uint8)
+    border_cond = 0
+    for i in range(1, width+1):
+        for j in range(1, height+1):
+            if tem_img[i-1,j] * tem_img[i+1,j] < 0:
+                if abs(tem_img[i-1,j] - tem_img[i+1,j]) >= d:
+                    border_cond += 1
+            if tem_img[i,j-1] * tem_img[i,j+1] < 0:
+                if abs(tem_img[i,j-1] - tem_img[i,j+1]) >= d:
+                    border_cond += 1
+            if tem_img[i+1,j+1] * tem_img[i-1,j-1] < 0:
+                if abs(tem_img[i+1,j+1] - tem_img[i-1,j-1]) >= d:
+                    border_cond += 1
+            if tem_img[i-1,j+1] * tem_img[i+1,j-1] < 0:
+                if abs(tem_img[i-1,j+1] - tem_img[i+1,j-1]) >= d:
+                    border_cond += 1
+            
+            if border_cond > 1:
+                res_img[i-1,j-1] = 255
+            border_cond = 0
+    return res_img
+
+def edge_detector(g_img, mode, threshold):
     if mode == "C":
-        return cv2.Canny(b_img, 50, 150)
+        return cv2.Canny(g_img, 50, 150)
     if mode == "R":
         conv_x = np.array( [[1, 0],
                             [0,-1]] )
@@ -63,26 +92,34 @@ def edge_detector(b_img, mode):
                            [0, 0, 0],
                            [-3, -10, -3]])
     elif mode=="L":
-        conv_x = np.array([[1, 1, 1],
+        conv = np.array([[1, 1, 1],
                            [1, -8, 1],
                            [1, 1, 1]])
-    
-        conv_y = np.array([[1, 1, 1],
-                           [1, -8, 1],
-                           [1, 1, 1]])
-        b_img = cv2.GaussianBlur(b_img, (3, 3), 0)
+        g_img = cv2.GaussianBlur(g_img, (3, 3), 0)
+        g_img = g_img.astype('float64')
+        LoG_img = ndimage.convolve(g_img, conv)
+        if  threshold >= 1:
+            threshold *= 0.01
+        d = threshold * LoG_img.max()
+        return find_zero_cross(LoG_img, d)
     else:
         raise ValueError("Incorrect mode argument!")
 
     
-    img = b_img.astype('float64')
+    img = g_img.astype('float64')
     vertical = ndimage.convolve(img, conv_x)
     horizontal = ndimage.convolve(img, conv_y)
+
+    res_img = np.zeros_like(img)
     
     edged_img = np.sqrt(np.square(horizontal) + np.square(vertical))
-    return edged_img.astype('uint8')
+    width, height = img.shape
+    for i in range(width):
+        for j in range(height):
+            res_img[i,j] = 255 if edged_img[i,j] > threshold else 0
+    return res_img.astype('uint8')
 
-def main(save_path, image_path, mode, grayscale):
+def main(save_path, image_path, mode, threshold):
     options = {
         "1": ("C", "Canny"),
         "2": ("R", "Roberts"),
@@ -116,15 +153,14 @@ def main(save_path, image_path, mode, grayscale):
     
     #mode check
     if mode == '0':
-        mode = "C"    
-    #grayscale chech
-    if grayscale == "0":
-        grayscale_str = ""
-        bin_image = get_bin_img(gray_image)
-    else:
-        grayscale_str = "grayscale"
-        bin_image = gray_image
+        mode = "C"
     
+    #threshold
+    try:
+        threshold = int(threshold)
+    except Exception as e:
+        raise e
+        
     timestamp = str(dt.timestamp(dt.now())).split('.')[-1]
     #create file_name string
     file_name = (
@@ -134,11 +170,12 @@ def main(save_path, image_path, mode, grayscale):
     )
     file_name = "_".join(
         (file_name,
-        grayscale_str,
+        "t"+str(threshold),
         options[mode][1])
     )
 
-    bin_img_proc = edge_detector(bin_image, options[mode][0])
+    #bin_image = get_bin_img(gray_image)
+    bin_img_proc = edge_detector(gray_image, options[mode][0], threshold)
     img = Image.fromarray(bin_img_proc)
     img.save(save_path+"/"+file_name+"_"+timestamp+".jpg")
 
@@ -158,9 +195,10 @@ if __name__ == "__main__":
             +">>>\t6. Laplace"
         )
         mode = input(">>> ")
-        grayscale = input("Use grayscale mode?[0-No] >>>\t")
+        #threshold
+        threshold = input("Enter threshold >>>\t")
         #main
-        main(save_path, image_path, mode, grayscale)
+        main(save_path, image_path, mode, threshold)
         stop = input("\nPress Enter to continue. To exit press 0 >>>\t")
         if stop == '0':
             print("\nProgram terminated...")
